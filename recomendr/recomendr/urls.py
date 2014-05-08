dffb2263859327efdc2d0794827b40b7756f46be
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-05-08 14:43:25 -0500

    Added a way to navigate to courses.

--- recomendr/recomendr/urls.py
+++ recomendr/recomendr/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'recomendr.recomendr.views.index', name='recomendr.index'),
-    url(r'^classes$', 'recomendr.recomendr.views.all_classes', name='recomendr.classes')
+    url(r'^courses/(?P<course_id>[0-9]+)', 'recomendr.recomendr.views.course_page', name='recomendr.course_page'),
+    url(r'^courses', 'recomendr.recomendr.views.all_courses', name='recomendr.courses')
 )
--- recomendr/recomendr/views.py
+++ recomendr/recomendr/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from recomendr.recomendr.models import *
 
 def index(request):
     return render(request, "recomendr/index.html", {})
     
-def all_classes(request):
+def all_courses(request):
     courses = Course.objects.all()
     return render(request, "recomendr/classes.html", {"classes": courses})
+
+def course_page(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, "recomendr/course.html", {"course": course})
