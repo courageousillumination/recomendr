208e9090512e64d80a01aeba41063e9bd489747f
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-05-09 18:59:05 -0500

    Added a typeahead search.

--- recomendr/recomendr/urls.py
+++ recomendr/recomendr/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'recomendr.recomendr.views.index', name='recomendr.index'),
     url(r'^courses/(?P<course_id>[0-9]+)', 'recomendr.recomendr.views.course_page', name='recomendr.course_page'),
-    url(r'^courses', 'recomendr.recomendr.views.all_courses', name='recomendr.courses')
+    url(r'^courses', 'recomendr.recomendr.views.all_courses', name='recomendr.courses'),
+    url(r'^search', 'recomendr.recomendr.views.search', name='recomendr.search')
 )
--- recomendr/recomendr/views.py
+++ recomendr/recomendr/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
 
 from recomendr.recomendr.models import *
 
 import recomendr.recomendr.lsi
 
+
 def index(request):
-    return render(request, "recomendr/index.html", {})
+    courses = Course.objects.all()
+    return render(request, "recomendr/index.html", {"courses" : courses})
     
 def all_courses(request):
     courses = Course.objects.all()
@@ -16,3 +18,13 @@ def course_page(request, course_id):
     similar_courses = recomendr.recomendr.lsi.get_similar_courses(course, 10)
     print similar_courses
     return render(request, "recomendr/course.html", {"course": course, "similar_courses": similar_courses})
+
+def search(request):
+    if request.method == "GET":
+        name = request.GET["course_name"]
+        courses = Course.objects.filter(title__icontains = name)
+        if len(courses) == 1:
+            return redirect('recomendr.course_page', course_id=courses[0].id)
+        return render(request, "recomendr/classes.html", {"classes": courses})
+        
+    return redirect('index')
